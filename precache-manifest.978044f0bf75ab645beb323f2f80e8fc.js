0d59c50ad4cf74907575afde26bd682b396bb8a4
unknown <pinlac.johncarlo@gmail.com>
2020-07-02 18:40:04 +0800

    Updates

---
+++ precache-manifest.978044f0bf75ab645beb323f2f80e8fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b347612e8ac53fd616190e9b5cd03f6",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "3bbb720a0a339baba3f9",
+    "url": "/monsters/static/css/main.714d1cd5.chunk.css"
+  },
+  {
+    "revision": "47bab0906028e34c06b7",
+    "url": "/monsters/static/js/2.51401e91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.51401e91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bbb720a0a339baba3f9",
+    "url": "/monsters/static/js/main.fc6e0bd5.chunk.js"
+  },
+  {
+    "revision": "53ad0c76a3375427c7e8",
+    "url": "/monsters/static/js/runtime-main.aa864354.js"
+  }
+]);
